f81dab0b8729af2698d874046bd58e9ac14e6089
Sarthak Duggal <52778617+sardug10@users.noreply.github.com>
2020-03-17 22:15:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  map = /* new google.maps.Map is a fucntion which takes in 2 parameters 
+          1st is the object that we have created and 2nd is some options
+             */ new google.maps.Map(document.getElementById('map'), {
     center: {lat: 28.7041, lng: 77.1025},
-    zoom: 8
+    zoom: 8 /* max zoom can be 14 */
   });
 }
